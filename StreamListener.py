09eb78c3f985d69935b2958519235c20ea8bd933
Blake Sanie <blake@sanie.com>
2019-05-20 15:48:21 -0700

    tweeting ability enabled, 5 min no-tweet window added for protection

--- StreamListener.py
+++ StreamListener.py
@@ -2,42 +2,36 @@ from Sentence import Sentence
 from utils import *
 import tweepy
 from Tweet import Tweet
-import json
+from threading import Timer
 
 class StreamListener(tweepy.StreamListener):
 
-    def on_connect(self):
-        self.queueLength = 0
+    # def on_connect(self):
+    # def on_limit(self, track):
 
-    # def on_data(self, raw_data):
-    #     """Called when raw data is received from connection.
-    #     Override this method if you wish to manually handle
-    #     the stream data. Return False to stop stream and close connection.
-    #     """
-    #     data = json.loads(raw_data)
-    #
-    #     if 'in_reply_to_status_id' in data:
-    #         status = tweepy.models.Status.parse(self.api, data)
-    #         if self.on_status(status) is False:
-    #             return False
-    #     else:
-    #         print("Unknown message type: " + str(raw_data))
-
-    def on_limit(self, track):
-        self.queueLength = track
-        # print("limit: {}".format(track))
+    def setApi(self, api):
+        self.api = api
+        self.canTweet = True
 
     def on_status(self, status):
-        #print(self.queueLength)
-        tweet = Tweet(status)
-        if tweet.isAcceptable():
-            sent = Sentence(tweet.cleaned)
-            if isPoem(sent):
-                print("\n\n")
-                print(formatPoem(sent))
-                print("\nAn accidental poem by @{}\n\n".format(tweet.user))
-                print(tweet.text)
+        if self.canTweet:
+            tweet = Tweet(status)
+            if tweet.isAcceptable():
+                sent = Sentence(tweet.cleaned)
+                if isPoem(sent):
+                    formatted = formatPoem(sent)
+                    print("\n\n")
+                    print("can Tweet")
+                    print(formatted)
+                    print("\nAn accidental poem by @{}\n\n".format(tweet.user))
+                    print(tweet.text)
+                    self.api.update_status("{}\nA life poem by @{}".formatPoem(formatted, tweet.user), tweet.id)
+                    self.canTweet = False
+                    timer = Timer(300.0, self.resetCanTweet)
+                    timer.start()
 
     def on_error(self, status_code):
         print("Status code: {}".format(status_code))
-        #return False
+
+    def resetCanTweet(self):
+        self.canTweet = True
