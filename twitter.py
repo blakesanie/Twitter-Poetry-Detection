86a009d3f9051bfb93db386d9ec93d2da0dee63e
Blake Sanie <blake@sanie.com>
2019-05-20 16:30:25 -0700

    env vars compatible

--- twitter.py
+++ twitter.py
@@ -1,9 +1,15 @@
 import keys
 import tweepy
 from StreamListener import StreamListener
+from os import environ
 
-auth = tweepy.OAuthHandler(keys.consumer_key, keys.consumer_secret)
-auth.set_access_token(keys.access_token, keys.access_token_secret)
+consumer_key = environ.get('consumer_key') if environ.get('consumer_key') is not None else keys.consumer_key
+consumer_secret = environ.get('consumer_secret') if environ.get('consumer_secret') is not None else keys.consumer_secret
+access_token = environ.get('access_token') if environ.get('access_token') is not None else keys.access_token
+access_token_secret = environ.get('access_token_secret') if environ.get('access_token_secret') is not None else keys.access_token_secret
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
 
 api = tweepy.API(auth)
 
