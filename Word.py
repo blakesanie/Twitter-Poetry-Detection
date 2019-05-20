b81b23f081d0df029302b809d50a164987c89bc6
Blake Sanie <blake@sanie.com>
2019-05-19 18:05:14 -0700

    save syllables and rhymes as instance vars, never have to obtain twice

--- Word.py
+++ Word.py
@@ -2,6 +2,8 @@ import nltk
 from nltk.corpus import cmudict
 
 cmu = cmudict.dict()
+entries = cmudict.entries()
+level = 2 #1 = semi rhyme, 2 = exact rhyme
 
 class Word:
 
@@ -9,6 +11,8 @@ class Word:
         self.text = text.replace('\n','')
         self.word = self.getWord(self.text)
         self.numSyllables = self.getNumSyllables(self.word)
+        self.rhymes = None #only compute these when needed, expensive process
+        self.syllables = None
 
     def getWord(self, text):
         for i, char in enumerate(text):
@@ -23,16 +27,18 @@ class Word:
             return 0 #not found
 
     def rhymesWith(self, wordObj):
-        entries = cmudict.entries()
-        syllables1 = [(word, syl) for word, syl in entries if word == self.word]
-        #syllables2 = [(word, syl) for word, syl in entries if word == wordObj.word]
-        rhymes1 = []
-        #rhymes2 = []
-        for (word, syllable) in syllables1:
-            rhymes1 += [word for word, pron in entries if pron[-2:] == syllable[-2:]]
-        #for (word, syllable) in syllables2:
-            #rhymes2 += [word for word, pron in entries if pron[-2:] == syllable[-2:]]
-        return wordObj.word in rhymes1# and self.word in rhymes2
+        if self.syllables is None:
+            syllables = [(word, syl) for word, syl in entries if word == self.word]
+            self.syllables = syllables
+        if self.rhymes is None:
+            rhymes = []
+            for (word, syllable) in self.syllables:
+                rhymes += [word for word, pron in entries if pron[-level:] == syllable[-level:]]
+            self.rhymes = rhymes
+        return wordObj.word in self.rhymes
+
+    def setRhymes(self, rhymes):
+        self.rhymes = rhymes
 
     def __str__(self):
         return str((self.text,self.word,self.numSyllables))
