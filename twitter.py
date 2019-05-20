15105173af5c5675788a4772b2a54f9a8622881d
Blake Sanie <blake@sanie.com>
2019-05-20 15:46:44 -0700

    give api to listener class, search for "life"

--- twitter.py
+++ twitter.py
@@ -8,5 +8,6 @@ auth.set_access_token(keys.access_token, keys.access_token_secret)
 api = tweepy.API(auth)
 
 streamListener = StreamListener()
+streamListener.setApi(api)
 stream = tweepy.Stream(auth = api.auth, listener=streamListener)
-stream.filter(track=["i"])
+stream.filter(track=["life"],languages=["en"]) # I dont want to fall behind
