e100f177ab003b844e21244ba4b5563c89dffc8b
Blake Sanie <blake@sanie.com>
2019-05-20 17:07:13 -0700

    new properties added

--- Tweet.py
+++ Tweet.py
@@ -1,13 +1,14 @@
 class Tweet:
 
     def __init__(self, status):
-        self.lang = status.lang
         self.text = status.text
+        self.truncated = status.truncated
+        self.id = status.id
         self.user = status.user.screen_name
         self.cleaned = self.cleaned()
 
     def isAcceptable(self):
-        return not any(char.isdigit() for char in self.cleaned) and not self.text[0:2] == "RT"
+        return not any(char.isdigit() for char in self.cleaned) and not self.text[0:2] == "RT" and not self.truncated
 
     def cleaned(self):
         indexOfAt = self.text.find("@")
