7e7c45bcc22b4aecc23aa6cf38a7e2fcb275933e
Blake Sanie <blake@sanie.com>
2019-05-21 14:29:29 -0700

    prints added back in for server work / monitoring

--- utils.py
+++ utils.py
@@ -1,5 +1,3 @@
-import sys
-
 def syllablesCompatable(totalSyllables):
     return totalSyllables % 4 == 0 and (totalSyllables / 4) >= 4
 
@@ -26,7 +24,7 @@ def aabbRhymes(wordsToRhyme):
     for i in range(0, len(wordsToRhyme), 2):
         if not wordsToRhyme[i].rhymesWith(wordsToRhyme[i+1]):
             return False
-        #print("\n{}\n".format([wordsToRhyme[i].word, wordsToRhyme[i+1].word]))
+        print("\n{}\n".format([wordsToRhyme[i].word, wordsToRhyme[i+1].word]))
     #print("aabb good")
     return True
 
@@ -34,7 +32,7 @@ def ababRhymes(wordsToRhyme):
     for i in range(len(wordsToRhyme) - 2):
         if not wordsToRhyme[i].rhymesWith(wordsToRhyme[i+2]):
             return False
-        #print("\n{}\n".format([wordsToRhyme[i].word, wordsToRhyme[i+2].word]))
+        print("\n{}\n".format([wordsToRhyme[i].word, wordsToRhyme[i+2].word]))
     #print("abab good")
     return True
 
@@ -54,7 +52,6 @@ def isPoem(sent):
     if not len(wordsToRhyme) == 4:
         return False
     print(list(word.word for word in wordsToRhyme))
-    sys.stdout.flush()
     return aabbRhymes(wordsToRhyme) or ababRhymes(wordsToRhyme) or abbaRhymes(wordsToRhyme)
 
 def formatPoem(sent):
