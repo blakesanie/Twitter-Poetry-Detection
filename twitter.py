160524ec79a16d4851398017393c583479d6aeab
Blake Sanie <blake@sanie.com>
2019-05-21 12:44:20 -0700

    cleaned up how to get keys

--- twitter.py
+++ twitter.py
@@ -1,17 +1,17 @@
-import sys
+onServer = False
 try:
     import keys
 except ImportError:
-    print("on server")
-    sys.stdout.flush()
+    onServer = True
+
 import tweepy
 from StreamListener import StreamListener
 from os import environ
 
-consumer_key = environ.get('consumer_key') if environ.get('consumer_key') is not None else keys.consumer_key
-consumer_secret = environ.get('consumer_secret') if environ.get('consumer_secret') is not None else keys.consumer_secret
-access_token = environ.get('access_token') if environ.get('access_token') is not None else keys.access_token
-access_token_secret = environ.get('access_token_secret') if environ.get('access_token_secret') is not None else keys.access_token_secret
+consumer_key = environ.get('consumer_key') if onServer else keys.consumer_key
+consumer_secret = environ.get('consumer_secret') if onServer else keys.consumer_secret
+access_token = environ.get('access_token') if onServer else keys.access_token
+access_token_secret = environ.get('access_token_secret') if onServer else keys.access_token_secret
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
